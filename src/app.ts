ef7067152f28fb881fe7136d030a7b773acceefe
Rodrigo Teixeira Ribeiro da Silva <rodrigoteix1998@gmail.com>
2024-02-12 21:36:41 -0300

    Creating folder structure, register use case and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
 import fastify from "fastify";
+import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
+
+app.register(appRoutes);
+
+app.setErrorHandler((err, _, res) => {
+	if (err instanceof ZodError) {
+		return res.status(400).send({ message: "Validation error.", issues: err.format() });
+	}
+
+	if (env.NODE_ENV !== "production") {
+		console.log(err);
+	} else {
+		// TODO Log to external tool like Datadog
+	}
+
+	return res.status(500).send({ message: "Internal server error."});
+});
---
+++ src/http/controllers/register.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply} from "fastify";
+import { RegisterUseCase } from "@/use-cases/register";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+
+export async function register (req: FastifyRequest, res: FastifyReply) {
+	
+	const registerBodySchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string().min(6)
+	});
+
+	const { name, email, password } = registerBodySchema.parse(req.body);
+
+	try {
+		const usersRepository = new PrismaUsersRepository();
+		const registerUseCase = new RegisterUseCase(usersRepository);
+		await registerUseCase.execute({
+			name, email, password
+		});
+	} catch (err) {
+		if (err instanceof UserAlreadyExistsError) {
+			return res.status(409).send({ message: err.message });
+		}
+
+		throw err;
+
+		return res.status(500).send();
+	}
+	
+
+	return res.status(201).send();
+}
