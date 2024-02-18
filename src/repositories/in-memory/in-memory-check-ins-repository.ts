ed848da315dd36d82962db2dd999b98910673434
Rodrigo Teixeira Ribeiro da Silva <rodrigoteix1998@gmail.com>
2024-02-18 15:24:36 -0300

    Adding checkin use case (incomplete)

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,34 @@
+import { CheckIn, Prisma } from "@prisma/client";
+import { CheckInsRepository } from "../check-ins-repository";
+import { randomUUID } from "node:crypto";
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+	public checkIns: CheckIn[] = [];
+
+	async findByUserIdOnDate(userId: string, date: Date) {
+		const checkInOnSameDate = this.checkIns.find(
+			(checkIn) => checkIn.user_id === userId
+		);
+
+		if (!checkInOnSameDate) {
+			return null;
+		}
+		console.log(date);
+
+		return checkInOnSameDate;
+	}
+	
+	async create(data: Prisma.CheckInUncheckedCreateInput) {
+		const checkIn = {
+			id: randomUUID(),
+			user_id: data.user_id,
+			gym_id: data.gym_id,
+			validated_at: data.validated_at ? new Date(data.validated_at) : null,
+			created_at: new Date()
+		};
+		
+		this.checkIns.push(checkIn);
+		
+		return checkIn;
+	}
+}
