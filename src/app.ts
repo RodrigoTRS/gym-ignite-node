2785296f88dc2d7276d8d59f2e15b98f0bd6c4d6
RodrigoTRS <rodrigoteix1998@gmail.com>
2024-02-27 12:00:36 -0300

    Adding JWT auth

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,27 @@ import fastify from "fastify";
 import { appRoutes } from "./http/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
 app.register(appRoutes);
 
 app.setErrorHandler((err, _, res) => {
-	if (err instanceof ZodError) {
-		return res.status(400).send({ message: "Validation error.", issues: err.format() });
-	}
+  if (err instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ message: "Validation error.", issues: err.format() });
+  }
 
-	if (env.NODE_ENV !== "production") {
-		console.log(err);
-	} else {
-		// TODO Log to external tool like Datadog
-	}
+  if (env.NODE_ENV !== "production") {
+    console.log(err);
+  } else {
+    // TODO Log to external tool like Datadog
+  }
 
-	return res.status(500).send({ message: "Internal server error."});
+  return res.status(500).send({ message: "Internal server error." });
 });
